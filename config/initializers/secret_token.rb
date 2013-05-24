92f391201d0bb10e8acbeee6b21b31292947e53b
nuex <nx@nu-ex.com>
2013-05-24 12:17:02 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoevents::Application.config.secret_token = '94fdaaa632fb3839b1037e126ab4e7773641f10dbe372435d4c672b671660c93c8f9fd02a4da71f2aa32e71214baa74d4e3fd1ecc0480e48524f0d1cd4c941ef'
