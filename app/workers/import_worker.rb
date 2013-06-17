1e272d090a62469dafc9cc1172454473a36d8843
nuex <nx@nu-ex.com>
2013-06-17 18:51:19 -0400

    delete file after import

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -2,10 +2,11 @@ require 'csv'
 class ImportWorker
   include Sidekiq::Worker
 
-  def perform(bucket_slug, path)
+  def perform(bucket_slug, file)
     if bucket = Bucket.find_by_slug(bucket_slug)
       bucket.events.destroy_all
-      CSV.foreach(path) { |r| import_row(bucket, r) }
+      CSV.foreach(file) { |r| import_row(bucket, r) }
+      delete(file)
       GeocodeWorker.perform_async(bucket.id)
     end
   end
@@ -18,5 +19,9 @@ class ImportWorker
     event.date = Date.strptime(row[1], '%Y-%m-%d')
     event.location = row[2]
     event.save
+  end
+
+  def delete(file)
+    File.delete(file)
   end
 end
