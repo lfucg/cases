2de62c0345af5f79f07cd2fe78e1f34de7c548ee
nuex <nx@nu-ex.com>
2013-06-17 19:24:44 -0400

    make importer handle more stuff

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -2,16 +2,32 @@ require 'csv'
 class ImportWorker
   include Sidekiq::Worker
 
-  def perform(bucket_slug, file)
-    if bucket = Bucket.find_by_slug(bucket_slug)
-      bucket.events.destroy_all
-      CSV.foreach(file) { |r| import_row(bucket, r) }
-      delete(file)
-      GeocodeWorker.perform_async(bucket.id)
+  def perform
+    each_import do |slug, file|
+      if bucket = Bucket.find_by_slug(slug)
+        bucket.events.destroy_all
+        CSV.foreach(file) { |r| import_row(bucket, r) }
+        delete(file)
+        GeocodeWorker.perform_async(bucket.id)
+      end
     end
   end
 
   private
+
+  def import_directory
+    @import_directory ||= "#{Rails.root}/import"
+  end
+
+  def each_import
+    Dir["#{import_directory}/*.csv"].each do |file|
+      yield(file_slug(file), file)
+    end
+  end
+
+  def file_slug(file)
+    File.basename(file, ".csv").downcase
+  end
 
   def import_row(bucket, row)
     event = bucket.events.new
