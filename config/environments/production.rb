3073cefaa114a821be9db2a1b7ffe64cb22fd705
nuex <nx@nu-ex.com>
2013-09-18 12:47:12 -0400

    possible fix for default from address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Geoevents::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'lex-geoevents@iostruct.com'}
+  config.action_mailer.default = {from: 'lex-geoevents@iostruct.com'}
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
