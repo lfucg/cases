01684be2c98f3aee6f464f5894fef5b76f259494
nuex <nx@nu-ex.com>
2013-10-31 01:33:40 -0400

    configure sidekiq redis db

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,6 +4,15 @@ require File.join(Rails.root, 'lib', 'yaml_config')
 module SidekiqInitializer
   def self.configure
     config = YamlConfig.load('sidekiq')
+
+    Sidekiq.configure_server do |cfg|
+      cfg.redis = { url: config[:redis_url] }
+    end
+
+    Sidekiq.configure_client do |cfg|
+      cfg.redis = { url: config[:redis_url] }
+    end
+
     Sidekiq::Web.use(Rack::Auth::Basic) do |u, p|
       [u, p] == [config[:user], config[:pass]]
     end
