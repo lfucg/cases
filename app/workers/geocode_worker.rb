5ea51593ecfa9099e0d04c1aead16fae71d0dd0a
nuex <nx@nu-ex.com>
2013-11-22 06:43:17 +0000

    use one worker for geocoding buckets and lower batch size to prevent process kill due to resource consumption

--- app/workers/geocode_worker.rb
+++ app/workers/geocode_worker.rb
@@ -3,17 +3,47 @@
 class GeocodeWorker
   include Sidekiq::Worker
 
+  # MapQuest Batch has a 100 address geocoding limit
+  BATCH_SIZE = 100
+
   def perform(bucket_id)
     bucket = Bucket.find(bucket_id)
-    bucket.events.geocodeable.find_in_batches do |b|
-      run_batch(b.map(&:hashed_address).uniq)
+    @remote_geocodeable = {}
+    @remote_geocodeable_count = 0
+
+    bucket.events.geocodeable.find_in_batches(batch_size: 10) do |batch|
+      batch.each { |e| geocode(e) }
     end
+    run_batch unless @remote_geocodeable.empty?
   end
 
   private
 
-  def run_batch(hashed_addresses)
-    GeocodeBatchWorker.perform_async(hashed_addresses)
+  def geocode(event)
+    maybe_run_batch
+    place = Place.where(hashed_address: event.hashed_address).first
+    place.present? ? event.sync_with_place(place) : maybe_add_to_batch(event)
+  end
+
+  def maybe_run_batch
+    if @remote_geocodeable_count == BATCH_SIZE
+      run_batch
+      @remote_geocodeable = {}
+      @remote_geocodeable_count = 0
+    end
+  end
+
+  def maybe_add_to_batch(event)
+    add_to_batch(event) unless @remote_geocodeable.key?(event.hashed_address)
+  end
+
+  def add_to_batch(event)
+    @remote_geocodeable[event.hashed_address] = event.address
+    @remote_geocodeable_count += 1
+  end
+  
+  def run_batch
+    GeocodeRemoteWorker.perform_async(@remote_geocodeable)
   end
 
 end
